726264b3493c48d3c2725b2aee3d7ff0fe7879be
udsmdev <114908345+udsmdev@users.noreply.github.com>
2022-11-08 11:44:01 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,170 @@
+new Vue({
+  el: "#app",
+  data() {
+    return {
+      audio: null,
+      circleLeft: null,
+      barWidth: null,
+      duration: null,
+      currentTime: null,
+      isTimerPlaying: false,
+      tracks: [
+        {
+          name: "Asian Trilogie 1",
+          artist: "Fjord",
+          cover: "https://github.com/Fjords-Tech/osbrncdbcbdfg/raw/main/IMG_1174.jpg",
+          source: "https://github.com/Fjords-Tech/osbrncdbcbdfg/raw/main/Asian%20Trilogie1.m4a",
+          url: "https://fjordservers.com",
+          favorited: true
+        },
+        {
+          name: "Asian Trilogie 2",
+          artist: "Fjord",
+          cover: "https://github.com/Fjords-Tech/osbrncdbcbdfg/raw/main/Screen%20Shot%202022-06-15%20at%206.50.50%20PM.png",
+          source: "https://github.com/Fjords-Tech/osbrncdbcbdfg/raw/main/Asian%20Trilogie2.m4a",
+          url: "https://fjordservers.com",
+          favorited: true
+        },
+        {
+          name: "Asian Trilogie 3",
+          artist: "Fjord",
+          cover: "https://github.com/Fjords-Tech/osbrncdbcbdfg/raw/main/Screen%20Shot%202022-06-16%20at%2011.21.48%20AM.png",
+          source: "https://github.com/Fjords-Tech/osbrncdbcbdfg/raw/main/Asian%20Trilogie3.m4a",
+          url: "https://fjordservers.com",
+          favorited: true
+        },
+                {
+          name: "A night in Egypt",
+          artist: "Fjord",
+          cover: "https://github.com/Fjords-Tech/osbrncdbcbdfg/raw/main/Music%20pro.jpeg",
+          source: "https://github.com/Fjords-Tech/osbrncdbcbdfg/raw/main/A%20night%20in%20Egypt.m4a",
+          url: "https://fjordservers.com",
+          favorited: true
+        },
+      ],
+      currentTrack: null,
+      currentTrackIndex: 0,
+      transitionName: null
+    };
+  },
+  methods: {
+    play() {
+      if (this.audio.paused) {
+        this.audio.play();
+        this.isTimerPlaying = true;
+      } else {
+        this.audio.pause();
+        this.isTimerPlaying = false;
+      }
+    },
+    generateTime() {
+      let width = (100 / this.audio.duration) * this.audio.currentTime;
+      this.barWidth = width + "%";
+      this.circleLeft = width + "%";
+      let durmin = Math.floor(this.audio.duration / 60);
+      let dursec = Math.floor(this.audio.duration - durmin * 60);
+      let curmin = Math.floor(this.audio.currentTime / 60);
+      let cursec = Math.floor(this.audio.currentTime - curmin * 60);
+      if (durmin < 10) {
+        durmin = "0" + durmin;
+      }
+      if (dursec < 10) {
+        dursec = "0" + dursec;
+      }
+      if (curmin < 10) {
+        curmin = "0" + curmin;
+      }
+      if (cursec < 10) {
+        cursec = "0" + cursec;
+      }
+      this.duration = durmin + ":" + dursec;
+      this.currentTime = curmin + ":" + cursec;
+    },
+    updateBar(x) {
+      let progress = this.$refs.progress;
+      let maxduration = this.audio.duration;
+      let position = x - progress.offsetLeft;
+      let percentage = (100 * position) / progress.offsetWidth;
+      if (percentage > 100) {
+        percentage = 100;
+      }
+      if (percentage < 0) {
+        percentage = 0;
+      }
+      this.barWidth = percentage + "%";
+      this.circleLeft = percentage + "%";
+      this.audio.currentTime = (maxduration * percentage) / 100;
+      this.audio.play();
+    },
+    clickProgress(e) {
+      this.isTimerPlaying = true;
+      this.audio.pause();
+      this.updateBar(e.pageX);
+    },
+    prevTrack() {
+      this.transitionName = "scale-in";
+      this.isShowCover = false;
+      if (this.currentTrackIndex > 0) {
+        this.currentTrackIndex--;
+      } else {
+        this.currentTrackIndex = this.tracks.length - 1;
+      }
+      this.currentTrack = this.tracks[this.currentTrackIndex];
+      this.resetPlayer();
+    },
+    nextTrack() {
+      this.transitionName = "scale-out";
+      this.isShowCover = false;
+      if (this.currentTrackIndex < this.tracks.length - 1) {
+        this.currentTrackIndex++;
+      } else {
+        this.currentTrackIndex = 0;
+      }
+      this.currentTrack = this.tracks[this.currentTrackIndex];
+      this.resetPlayer();
+    },
+    resetPlayer() {
+      this.barWidth = 0;
+      this.circleLeft = 0;
+      this.audio.currentTime = 0;
+      this.audio.src = this.currentTrack.source;
+      setTimeout(() => {
+        if(this.isTimerPlaying) {
+          this.audio.play();
+        } else {
+          this.audio.pause();
+        }
+      }, 300);
+    },
+    favorite() {
+      this.tracks[this.currentTrackIndex].favorited = !this.tracks[
+        this.currentTrackIndex
+      ].favorited;
+    }
+  },
+  created() {
+    let vm = this;
+    this.currentTrack = this.tracks[0];
+    this.audio = new Audio();
+    this.audio.src = this.currentTrack.source;
+    this.audio.ontimeupdate = function() {
+      vm.generateTime();
+    };
+    this.audio.onloadedmetadata = function() {
+      vm.generateTime();
+    };
+    this.audio.onended = function() {
+      vm.nextTrack();
+      this.isTimerPlaying = true;
+    };
+    
+    for (let index = 0; index < this.tracks.length; index++) {
+      const element = this.tracks[index];
+      let link = document.createElement('link');
+      link.rel = "prefetch";
+      link.href = element.cover;
+      link.as = "image"
+      document.head.appendChild(link)
+    }
+  }
+});
